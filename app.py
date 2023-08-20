23b2ec090af81b8799f6dd85b722878bf73ed752
makkathran <markfarkas94@gmail.com>
2023-08-20 19:07:28 +0200

    WeatherApp Web Application (Flask)
    Weather App
    Description: A web application that allows users to view weather and air quality data based on their current location. The application uses Flask for the backend, SQLite for data storage, and Bootstrap for the frontend.
    
    Features:
    User Registration & Authentication: Users can register for an account and log in.
    Weather Data: Once logged in, users can view weather data for their current location.
    Air Quality Data: Along with weather details, users can also view air quality data.
    Responsive UI: Leveraging Bootstrap, the application provides a clean and responsive user interface.
    Directory Structure & Files:
    app.py: The main Flask application file containing routes and application configurations.
    models.py: Defines the database structure using Flask-SQLAlchemy. Currently, it has a User model.
    forms.py: Contains definitions for the registration and login forms using Flask-WTF.
    base.html: The base template providing a consistent layout for other templates.
    index.html: The main page, which displays weather and air quality data.
    login.html: The user login page.
    registers.html: The user registration page.
    style.css: Stylesheet to enhance the frontend appearance.
    weather.db: SQLite database file for data storage.
    External Integrations:
    OpenWeatherMap: Used to fetch weather data.
    Nominatim (OpenStreetMap): Used to derive city names from latitude and longitude coordinates.
    Setup:
    Clone the repository.
    Install the required packages (ensure you have a requirements.txt or specify them).
    Run the application: python app.py
    Open a browser and navigate to http://localhost:5000
    Contributions:
    Feel free to fork this repository and contribute. Pull requests are welcome!

---
+++ app.py
@@ -0,0 +1,137 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from models import User, create_tables, db
+from forms import RegistrationForm, LoginForm
+import requests
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+# Database initialization
+db.init_app(app)
+
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route("/set_location", methods=['POST'])
+def set_location():
+    data = request.json
+    latitude = data.get("latitude")
+    longitude = data.get("longitude")
+
+    city = get_city_from_coordinates(latitude, longitude)
+    weather_data = get_weather_data(city)
+    air_quality_data = get_air_quality_data(city)
+
+    return jsonify({"weather_data": weather_data, "air_quality_data": air_quality_data, "city": city})
+
+
+def get_city_from_coordinates(latitude, longitude):
+    nominatim_url = f"https://nominatim.openstreetmap.org/reverse?lat={latitude}&lon={longitude}&format=json"
+    response = requests.get(nominatim_url)
+    data = response.json()
+    city = data.get("address", {}).get("city", "")
+    return city
+
+
+def get_weather_data(city):
+    api_key = '7e6d29e7cec01c14ae4af476c7b254f8'
+    base_url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'q': city, 'appid': api_key, 'units': 'metric'}
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if response.status_code == 200:
+        weather_data = {
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+            'city': city
+        }
+        return weather_data
+
+    return None
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Thank you for registering!', 'success')
+        return redirect(url_for('login'))
+    return render_template('registers.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/air_quality/<city>')
+@login_required
+def air_quality(city):
+    air_quality_data = get_air_quality_data(city)
+    return render_template('air_quality.html', air_quality_data=air_quality_data, city=city)
+
+
+def get_air_quality_data(city):
+    api_key = '6a68a802-e6b9-4597-ba44-6991e15c7787'
+    base_url = 'https://api.airvisual.com/v2/nearest_city?key=6a68a802-e6b9-4597-ba44-6991e15c7787'
+    params = {'city': city, 'key': api_key}
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if response.status_code == 200:
+        air_quality_data = {
+            'aqi': data['data']['current']['pollution']['aqius'],
+            'main_pollutant': data['data']['current']['pollution']['mainus']
+        }
+        return air_quality_data
+
+    return None
+
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        create_tables()
+    app.run(debug=True)
+
+
+
---
+++ models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return str(self.id)
+
+    def is_authenticated(self):
+        return True
+
+def create_tables():
+    db.create_all()
+
+
+def user():
+    return None
